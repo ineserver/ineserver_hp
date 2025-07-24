e8a5d84bb87f039a2a9a748d224b4c9fb13d543d
1necat <ine0056@gmail.com>
2025-07-24 10:11:17 +0900

    ビルド時のエラーを修正

--- src/components/ServerStatus.tsx
+++ src/components/ServerStatus.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect, useCallback } from 'react';
-import Link from 'next/link';
 import { ServerStatus as ServerStatusType } from '@/types/server-status';
 import MaintenanceSchedule from './MaintenanceSchedule';
 
