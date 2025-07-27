1674604593aa8c797d458a3c69a16dae75758b4a
1necat <ine0056@gmail.com>
2025-07-28 00:07:09 +0900

    Remove unused archive and test files

--- test-new-parser.mjs
+++
@@ -1,6 +0,0 @@
-import { parseFrontMatter } from './lib/front-matter-parser.js';
-import fs from 'fs';
-
-const content = fs.readFileSync('./content/patch-notes/1-0.md', 'utf8');
-const result = parseFrontMatter(content);
-console.log('Parsed data:', JSON.stringify(result.data, null, 2));
