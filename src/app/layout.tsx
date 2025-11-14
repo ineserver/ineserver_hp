883fe2e928e383814da00e95854491a0e9363ca5
ine-macbookair <ine0056@gmail.com>
2025-11-15 01:05:02 +0900

    フォントが正しく表示されない問題を修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
+import { Noto_Sans_JP } from "next/font/google";
+
+  const noto = Noto_Sans_JP({
+    subsets: ["latin"],
+    weight: ["400","500","700"],
+    display: "swap",
+  });
 
 export const metadata: Metadata = {
   title: "いねさば",
@@ -13,7 +20,7 @@ export const viewport: Viewport = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
-      <body className="antialiased">
+      <body className="noto">
         {children}
       </body>
     </html>
